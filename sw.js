09dd249efe0bb0db42ff423418ea9c52f2626b1d
Lucas <emmyluc07@gmail.com>
2026-01-07 12:32:06 +0100

    v2.2.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hustleos-v1.0.0';
+const CACHE_NAME = 'hustleos-v2.2.0';
 const urlsToCache = [
   './',
   './index.html',
@@ -9,9 +9,26 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Force new SW to activate immediately
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Delete old caches
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim(); // Take control of all pages
+    })
   );
 });
 
